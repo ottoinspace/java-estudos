fd0c19a5c1fc4a7f2492507ae525e3b2877c9b07
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-09-11 21:05:17 -0300

    aula 07

---
+++ segundo semestre/teste/src/negocio/testeUm.java
@@ -0,0 +1,20 @@
+package negocio;
+
+public class testeUm {
+    private String algumaCoisa = "";
+
+    public testeUm() {
+    }
+
+    public testeUm(String algumaCoisa) {
+        this.algumaCoisa = algumaCoisa;
+    }
+
+    public String getAlgumaCoisa() {
+        return algumaCoisa;
+    }
+
+    public void setAlgumaCoisa(String algumaCoisa) {
+        this.algumaCoisa = algumaCoisa;
+    }
+}
